659c6fc5ac0d248c3201ebbdf08827728e9ec689
g1pd78 <g1pd78@gmail.com>
2021-07-28 18:08:31 +0500

    Get and send messages

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
+ListOfMessages = []
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Messenger Flask server is running! ' \
+           '<br> <a href="/status">Check status</a>'
 
+@app.route('/status')
+def status():
+    return {
+        'messages_count': len(ListOfMessages)
+    }
+# отправка сообщений
+@app.route("/api/Messanger", methods=['POST'])
+def SendMessage():
+    msg = request.json
+    print(msg)
+    ListOfMessages.append(msg)
+    msgtext = f"{msg['UserName']} <{msg['TimeStamp']}>: {msg['MessageText']}"
+    print(f"Всего сообщений: {len(ListOfMessages)} Посланное сообщение: {msgtext}")
+    return f"Сообщение отослано успешно. Всего сообщений: {len(ListOfMessages)} ", 200
+
+# получение сообщений
+@app.route("/api/Messanger/<int:id>")
+def GetMessage(id):
+    print(id)
+    if id >= 0 and id < len(ListOfMessages):
+        print(ListOfMessages[id])
+        return ListOfMessages[id], 200
+    else:
+        return "Not found", 400
 
 if __name__ == '__main__':
     app.run()
